53aa89ef07c89220c2281813a292cdeb36f811e9
“KseGreb” <ksegreb@gmail.com>
2023-08-27 16:42:50 -0700

    initial commit

---
+++ src/Accordion/accordion.js
@@ -0,0 +1,11 @@
+import { AccordionItem } from "./accotdionitem"
+import {list} from "./list"
+import "./style.css"
+
+export const Accordion = () => {
+    return(<div>
+        {list.map((item, index) =>
+        <AccordionItem key={index} title={item.title} content={item.content}/>
+        )}
+    </div>)
+}
